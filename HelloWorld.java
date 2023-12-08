3afa3011488d818498fd90669daae2c565859f0a
ditiss <ditiss@students.acts.in>
2023-12-08 16:48:10 +0530

    first commit

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World!");
+		System.out.println("This is a Docker image");
+		System.out.println("Cdac-Ditiss Copyright");
+		System.out.println("kuch aur");
+	}
+}
